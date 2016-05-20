38512b3f30bdf744853a3be28e473e37eb7e92c2
Jonah Monaghan <naruichia@gmail.com>
2016-05-20 15:06:52 -0400

    Added State Classes
    Please add these files to the setting up package

---
+++ Menu.java
@@ -0,0 +1,40 @@
+package settingup;
+
+//Imports
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+/**
+ *
+ * @author Jonah Monaghan
+ * @version 1.00
+ */
+public class Menu extends BasicGameState {
+
+    public Menu(int state){
+        
+    }
+    
+    
+    @Override
+    public int getID() {
+        return 0;
+    }
+
+    @Override
+    public void init(GameContainer gc, StateBasedGame sbg) throws SlickException {
+        
+    }
+
+    @Override
+    public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException {
+        //Random background generator [method]
+        
+    }
+
+    @Override
+    public void update(GameContainer gc, StateBasedGame sbg, int i) throws SlickException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
