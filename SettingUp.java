4b52784384f5fde59b6632dd8756df20bcff374c
Matthew Godfrey (Memai Saru) <matthewdgodfrey@gmail.com>
2016-05-31 21:47:36 -0400

    Add files via upload

--- SettingUp.java
+++ SettingUp.java
@@ -36,7 +36,7 @@ public class SettingUp extends StateBasedGame {
         this.getState(menu).init(gc, this);
         this.getState(play).init(gc, this);
         this.getState(credits).init(gc, this);
-        this.enterState(credits);
+        this.enterState(menu);
     }
    
     public static void main(String[] args) throws SlickException {
